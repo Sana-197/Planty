2c42c9083f35288fe9353b235d9e04c2ec2229af
Sana-197 <lilou-sb@hotmail.fr>
2024-01-11 13:40:38 +0100

    Header and footer, css modifications

---
+++ app/public/wp-content/themes/hello-elementor-child/footer.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the body & html closing tags.
+ *
+ * @package HelloElementor
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+?>
+<?php wp_footer(); ?>
+
+	<footer>
+		<div class="footer">
+			<a href="">Mentions légales</a>
+		</div>
+	</footer>
+
+</body>
+</html>
