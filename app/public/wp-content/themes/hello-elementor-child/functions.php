19304ab63d89f6e3e47ad7d2db61f8626b9bbf6a
Sana-197 <lilou-sb@hotmail.fr>
2024-01-16 19:17:56 +0100

    hook for admin menu in functions and adjustments in style.css

--- app/public/wp-content/themes/hello-elementor-child/functions.php
+++ app/public/wp-content/themes/hello-elementor-child/functions.php
@@ -7,3 +7,18 @@ function enqueue_child_theme_styles() {
     wp_enqueue_style('child-style', get_stylesheet_uri(), array('parent-style'), filemtime(get_stylesheet_directory() . '/style.css'));
     // charge le style du thème enfant et utilise le paramètre filemtime pour ajouter une version basée sur la dernière modification du fichier thème enfant.
 }
+//Hook pour le lien admin//
+
+function add_admin_link_to_menu( $items, $args ) {
+    
+    if ( is_user_logged_in() && $args->theme_location == 'menu-1' ) {
+        
+        $items .= '<li id="menu-item-admin" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-admin"><a href="' . admin_url() . '">Admin</a></li>';
+       
+    }
+
+    return $items;
+}
+
+add_filter( 'wp_nav_menu_items', 'add_admin_link_to_menu', 10, 2 );
+
